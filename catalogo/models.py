51ef63b96e798ef50f037eb5b31c38363cd2bcf9
joaogabriel33 <jgliraleite@gmail.com>
2026-08-29 16:43:19 -0300

    feat: cria models Plataforma, Genero, Jogo e Avaliacao

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Plataforma(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Genero(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Jogo(models.Model):
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField(blank=True)
+    data_lancamento = models.DateField(null=True, blank=True)
+    plataformas = models.ManyToManyField(Plataforma, related_name='jogos')
+    generos = models.ManyToManyField(Genero, related_name='jogos')
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+
+
+class Avaliacao(models.Model):
+    jogo = models.ForeignKey(Jogo, on_delete=models.CASCADE, related_name='avaliacoes')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='avaliacoes')
+    nota = models.PositiveSmallIntegerField()
+    comentario = models.TextField(blank=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('jogo', 'usuario')
+
+    def __str__(self):
+        return f'{self.usuario} avaliou {self.jogo} com nota {self.nota}'
