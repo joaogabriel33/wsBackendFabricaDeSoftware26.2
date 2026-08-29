ea6d6d6cfab7c36e369a749ba2c8cb60a6ce72b4
joaogabriel33 <jgliraleite@gmail.com>
2026-08-29 18:59:28 -0300

    feat: adiciona formulario de avaliacao de jogos

---
+++ catalogo/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Avaliacao
+
+
+class AvaliacaoForm(forms.ModelForm):
+    class Meta:
+        model = Avaliacao
+        fields = ['jogo', 'nota', 'comentario']
